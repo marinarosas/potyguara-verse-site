4e4f6d41a0afe03d6ed6290e026a4267cba595a7
Marina Jaudy <marinajaudy@MacBook-Pro-de-Marina.local>
2024-02-12 22:07:00 -0300

    feat: Create login page, web layout is ready

---
+++ src/app/app/login-page/page.tsx
@@ -0,0 +1,79 @@
+import { Button } from "@/components/ui/button";
+
+export default function Login() {
+  return (
+    <main className="h-screen text-foreground bg-background border-2 border-orange-logo">
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 border-2 border-green-neon">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm border-2 border-purple">
+          <img
+            className="h-auto w-36 m-auto"
+            src="/LogoRetangular.png"
+            alt="Potyguara Logo"
+          />
+          <h2 className="text-center text-xl font-bold leading-4 tracking-tight text-gray-900">
+            Entre na sua conta
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                E-mail
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+                />
+              </div>
+            </div>
+
+            <div>
+              <Button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-logo focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </Button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -41,8 +41,8 @@ const products = [
   },
 ];
 const callsToAction = [
-  { name: "Watch demo", href: "#", icon: PlayCircleIcon },
-  { name: "Contact sales", href: "#", icon: PhoneIcon },
+  { name: "Assita o trailer", href: "#", icon: PlayCircleIcon },
+  { name: "Fale conosco", href: "#", icon: PhoneIcon },
 ];
 
 function classNames(...classes: (string | boolean | undefined | null)[]): string {
@@ -240,6 +240,7 @@ export function Header() {
           <a
             href="#"
             className="text-sm font-semibold leading-6 text-gray-900 hover:text-orange-md"
+            onClick={()=>handleNavigateToLoginPage()}
           >
             Log in <span aria-hidden="true">&rarr;</span>
           </a>
@@ -249,12 +250,12 @@ export function Header() {
       {/* MOBILE */}
       <Dialog
         as="div"
-        className="lg:hidden"
+        className="lg:hidden bg-background"
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 bg-background text-foreground">
           <div className="flex items-center justify-end">
             <button
               type="button"
@@ -325,6 +326,7 @@ export function Header() {
                 <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:text-orange-md"
+                  onClick={()=>handleNavigateToLoginPage()}
                 >
                   Log in
                 </a>
